175b92c895df33723990da704a972029c617fb57
Al1Bi2 <gggvvvv576@gmail.com>
2025-03-18 16:09:00 +0300

    redo 3 and 4

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -20,31 +20,30 @@ void loop() {
   // Чтение значения с потенциометра (диапазон от 0 до 1023)
   int potValue = analogRead(potPin);
 
-
   // Вывод значения на монитор порта
   Serial.print("Потенциометр: ");
   Serial.println(potValue);
 
-  //Задаём ширину каждого сектора потенциометра(1026 для того чтобы при взятии модуля от 1023 не было 1)
-  int maxled = 1026/3;
-  // Преобразование значения с потенциометра в диапазон от 0 до 255
-  int ledvalue = map(potValue%(maxled),0,maxled,0,255);
-  //Выбор пина в зависимости от значения потенциометра
-  int ledpin = potValue/(maxled);
+  // Задаём ширину каждого сектора потенциометра
+  int maxled = 1023 / 3;
+
+  // Выбор пина в зависимости от значения потенциометра
+  int ledpin = potValue / maxled;
+
   // Управление светодиодами в зависимости от значения потенциометра
-  if (ledpin ==0) {  // Значение около 0
-    analogWrite(ledPin1, ledvalue);   
-    digitalWrite(ledPin2, LOW);  
-    digitalWrite(ledPin3, LOW);   
-  } else if (ledpin==1) {  // Значение около ½
-    analogWrite(ledPin2, ledvalue);   
-    digitalWrite(ledPin1, LOW); 
-    digitalWrite(ledPin3, LOW);  
+  if (ledpin == 0) {  // Значение около 0
+    digitalWrite(ledPin1, HIGH);  // Включаем красный
+    digitalWrite(ledPin2, LOW);   // Выключаем зеленый
+    digitalWrite(ledPin3, LOW);   // Выключаем синий
+  } else if (ledpin == 1) {  // Значение около ½
+    digitalWrite(ledPin2, HIGH);  // Включаем зеленый
+    digitalWrite(ledPin1, LOW);   // Выключаем красный
+    digitalWrite(ledPin3, LOW);   // Выключаем синий
   } else {  // Значение около 1
-    analogWrite(ledPin3, ledvalue);   
-    digitalWrite(ledPin1, LOW);  
-    digitalWrite(ledPin2, LOW);   
+    digitalWrite(ledPin3, HIGH);  // Включаем синий
+    digitalWrite(ledPin1, LOW);   // Выключаем красный
+    digitalWrite(ledPin2, LOW);   // Выключаем зеленый
   }
 
-  delay(50); 
-} 
+  delay(50);  // Пауза для стабилизации
+}
--- lab4/lab4.ino
+++ lab4/lab4.ino
@@ -2,35 +2,34 @@ int redPin = 9;   // Пин для красного цвета
 int greenPin = 10; // Пин для зеленого цвета
 int bluePin = 11;  // Пин для синего цвета
 
+// Массив RGB значений для 7 цветов радуги (русский порядок)
+int rainbowColors[7][3] = {
+  {255, 0, 0},     // Красный
+  {255, 69, 0},   // Оранжевый
+  {255, 255, 0},   // Желтый
+  {0, 255, 0},     // Зеленый
+  {0, 255, 255},   // Голубой
+  {0, 0, 255},     // Синий
+  {255, 0, 255}  // Фиолетовый
+};
+
 void setup() {
   // Устанавливаем пины как выходы
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
-  //На всякий случай устанавливаем 0 на выводах
+  // На всякий случай устанавливаем 0 на выводах
   analogWrite(redPin, 0);  
   analogWrite(greenPin, 0);  
   analogWrite(bluePin, 0);  
 }
 
 void loop() {
-  //Переход от красного к зеленому
-  for (int i = 255; i >=0; i--) {
-    analogWrite(redPin, i);        
-    analogWrite(greenPin, 255 - i); 
-    delay(15);
+  // Переход по всем цветам радуги
+  for (int i = 0; i < 7; i++) {
+    analogWrite(redPin, rainbowColors[i][0]);
+    analogWrite(greenPin, rainbowColors[i][1]);
+    analogWrite(bluePin, rainbowColors[i][2]);
+    delay(1000); // Задержка 1 секунда на каждом цвете
   }
-  //Переход от зеленого к синему
-  for (int i = 255; i >=0; i--) {   
-    analogWrite(greenPin, i); 
-    analogWrite(bluePin, 255-i);        
-    delay(15);
-  }
-  //Переход от синего к красному
-  for (int i = 255; i >=0; i--) {
-    analogWrite(redPin, 255-i);        
-    analogWrite(bluePin, i);    
-    delay(15); 
-  }
-
 }
